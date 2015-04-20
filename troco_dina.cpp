b82dadae4334b9e53dc760afe4fa18cb5b7ee28d
Pedro Dias <pedrodias@MacBook-Pro-de-Pedro-5.local>
2015-04-20 12:06:53 +0100

    ale ale

---
+++ troco_dina.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int troco(int valor,map<int,int> &moedas){
+	int min,aux,i;
+	int cache[valor+1];
+	int moeda[valor+1];
+
+	cache[0]=0;
+	ale lae
+}
+
+
+
+
+
+int main(){
+	int v;
+	map<int,int> moedas;
+	cin >> v;
+	cout << troco(v,moedas) << endl;
+	cout << "1: "<< moedas[1]<< endl;
+	cout << "3: "<< moedas[3]<< endl;
+	cout << "4: "<< moedas[4]<< endl;
+	return 0;
+}
